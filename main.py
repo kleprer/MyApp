e8ea362d761874d950fe363bcfc6ae80b74b9c5c
kleprer <kleprer@gmail.com>
2023-10-01 20:04:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import wikipedia
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Read_Summary(BaseModel):
+    page_title: str
+
+class Get_Image(BaseModel):
+    page_title: str
+    image_url: str
+
+
+@app.get("/{page_title}")
+def get_summary(page_title: str):
+    return wikipedia.summary(page_title)
+
+@app.get("/")
+def get_page_url(page_title: str):
+    return wikipedia.page(page_title).url
+
+@app.post("/", response_model=Get_Image)
+def image(get_image: Get_Image):
+    return Get_Image(page_title=get_image.page_title, image_url=wikipedia.page(get_image.page_title).images[0])
+
+
+
+
+
