ef473d19acea14a56014ee855dc32e76dbf955da
Снежана Максимова <146105343+kleprer@users.noreply.github.com>
2023-10-02 08:59:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,19 +4,34 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-#не придумала как заюзать и вообще как валидировать запросы, при любой попытке 
+#не придумала как заюзать и вообще как валидировать запросы, при любой попытке ниже
 #в swagger появляется response 500 internal server error
 class Read_Summary(BaseModel):
     page_title: str
+    page_summary: str
+
+class Page_Url(BaseModel):
+    page_title: str
+    page_url: str
 
 class Get_Image(BaseModel):
     page_title: str
     image_url: str
 
 
+#попытка валидации (неудачная)
+#@app.get("/{page_title}", response_model = Read_Summary)
+#def get_summary(page_summary: Read_Summary):
+#    return Read_Summary(page_title = page_summary.page_title, page_summary = wikipedia.summary(page_title))
+
 @app.get("/{page_title}")
 def get_summary(page_title: str):
     return wikipedia.summary(page_title)
+
+#попытка валидации (неудачная)
+#@app.get("/", response_model = Page_Url)
+#def get_page_url(page_url: Page_Url):
+#    return Page_Url(page_title = page_url.page_title, page_url = wikipedia.page(page_url.page_title).images[0])
 
 @app.get("/")
 def get_page_url(page_title: str):
@@ -28,5 +43,19 @@ def image(get_image: Get_Image):
 
 
 
-
+#по адресу http://127.0.0.1:8000/ выдает ошибку, не разобралась, как ее решить:
+#{
+#    "detail": [
+#        {
+#            "type": "missing",
+#            "loc": [
+#                "query",
+#                "page_title"
+#            ],
+#            "msg": "Field required",
+#            "input": null,
+#            "url": "https://errors.pydantic.dev/2.4/v/missing"
+#        }
+#    ]
+#}
 
